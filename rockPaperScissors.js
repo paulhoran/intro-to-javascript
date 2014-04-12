39ffa18c7de87564caaa6b80c4624deda0366ad5
paulhoran <phoran@me.com>
2014-04-12 12:33:18 -0700

    Create rockPaperScissors.js

---
+++ rockPaperScissors.js
@@ -0,0 +1,62 @@
+function play() {
+  console.log("Get ready!");
+  var playerChoice = prompt("Rock, Paper or Scissors?");
+  var computerChoice = Math.random();
+  
+  if (computerChoice < 0.33) {
+    computerChoice = "rock";
+  } else if (computerChoice > 0.66) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  
+  console.log("The player chose " + playerChoice);
+  console.log("The computer chose " + computerChoice);
+  
+  winner(playerChoice, computerChoice);
+}
+
+function winner (choice1, choice2) {
+  if (choice1 === choice2) {
+    console.log("Its a Tie!");
+    return;
+  }
+  if (choice1 === "rock") {
+    if (choice2 === "paper") {
+      console.log("Paper Wins!");
+    } else {
+      console.log("Rock Wins!");
+    }
+    if (choice2 === "paper") {
+    } else {
+    }  
+ }
+  if (choice1 === "paper") {
+    if (choice2 === "rock") {
+      console.log("Paper Wins!");
+    } else {
+      console.log("Scissors Wins!");
+   {
+    }  
+  }
+   }
+  if (choice1 === "scissors") {
+    if (choice2 === "paper") {
+      console.log("Scissors Wins!");
+    } else {
+      console.log("Rock Wins!");
+   {
+    }  
+  }
+    
+  }
+}
+console.clear(); 
+
+
+var readyToPlay = prompt("Would you like to play Rock, Paper, Scissors?");
+
+if (readyToPlay === "yes") {
+  play();
+}
